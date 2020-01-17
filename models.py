8d74cf88c5b047c590b5b4df6fb6a9a835d447ae
seanpohyx <seanpohyx@gmail.com>
2020-01-17 16:20:27 +0800

    clean code

--- models.py
+++ models.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm 
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import InputRequired, Email, Length, DataRequired, EqualTo
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -62,37 +58,3 @@ class Answer(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Login')
-
-class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-class UpdateForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
-    retypePassword = PasswordField('Retype Password', validators=[InputRequired(), Length(min=8, max=80)])
-    image = FileField('Update Profile Picture', validators=[FileAllowed(['png','jpg','jpeg'])])
-    submit = SubmitField('Submit')
-    
-class PostQuestionForm(FlaskForm):
-    question_title = StringField('Title', validators=[InputRequired(), Length(min=4, max=100)])
-    question = TextAreaField('Question', render_kw={"rows": 3}, validators=[InputRequired(), Length(max=1000)])
-    submit = SubmitField('Submit')
-
-class SearchForm(FlaskForm):
-    submit = SubmitField('Next')
-
-class AnswerForm(FlaskForm):
-    answer = TextAreaField(render_kw={"rows": 3}, validators=[InputRequired(),Length(max=1000)])
-    submit = SubmitField('Submit')
-
